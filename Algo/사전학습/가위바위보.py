68fb789921a2aa58309a32e511db8bffe9c869b5
Sooji Kwon <gkdrhd6788@gmail.com>
2023-07-14 17:40:50 +0900

    Before Going Home

---
+++ Algo/사전학습/가위바위보.py
@@ -0,0 +1,14 @@
+A,B=map(int, input().split())
+
+
+if (A !=2) and (B !=2):
+    if A == 1:
+        print('A')
+    else :
+        print('B')
+else:
+    if A > B:
+        print('A')
+    else:
+        print('B')
+        
