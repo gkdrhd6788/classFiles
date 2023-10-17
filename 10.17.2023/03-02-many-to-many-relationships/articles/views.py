719db73fded595f17cff49e4e5257e385969de98
Sooji Kwon <gkdrhd6788@gmail.com>
2023-10-17 17:57:04 +0900

    did it

--- 10.17.2023/03-02-many-to-many-relationships/articles/views.py
+++ 10.17.2023/03-02-many-to-many-relationships/articles/views.py
@@ -95,16 +95,10 @@ def comments_delete(request, article_pk, comment_pk):
     return redirect('articles:detail', article_pk)
 
 
-def likes(request,article_pk):
+def likes(request, article_pk):
     article = Article.objects.get(pk=article_pk)
-    # 현재 좋아요 버튼을 누른 유저가 이미 있으면, 취소
     if request.user in article.like_users.all():
         article.like_users.remove(request.user)
-        # 이렇게 해도 됨
-        # request.user.like_articles.remove(article)
     else:
-        # 추가
         article.like_users.add(request.user)
-        # request.user.like_articles.add(articles)
     return redirect('articles:index')
-
